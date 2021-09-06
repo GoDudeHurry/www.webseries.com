38c520dd79a6cad4a659f920f55348fa641f6bca
GoDudeHurry <88720932+GoDudeHurry@users.noreply.github.com>
2021-09-06 19:25:46 +0530

    First enty

---
+++ webseries.js
@@ -0,0 +1,20 @@
+document.getElementsByClassName('name')
+document.getElementsByClassName('watched')
+document.getElementsByClassName('active')
+document.getElementsByTagName('a')
+function reload()
+{
+    alert('Do you want to reload this page.');
+}
+function watched()
+{
+    alert('Work in progress!\nPlease try again later.');
+}
+function home()
+{
+    alert('Do you really wish to reload the current page?');
+}
+function stream()
+{
+    alert('Do you wish to continue\n(This action leads to external window)\n!!!OPEN WITH CAUTION!!!')
+}
